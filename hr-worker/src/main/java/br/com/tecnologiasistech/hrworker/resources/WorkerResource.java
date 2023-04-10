1129118531e7d05260b5694b825f6652f9387178
Uériclis Lima <uericlislima@gmail.com>
2023-04-10 14:42:50 -0300

    projeto hrpayroll created

--- hr-worker/src/main/java/br/com/tecnologiasistech/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/tecnologiasistech/hrworker/resources/WorkerResource.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.tecnologiasistech.hrworker.entities.Worker;
 import br.com.tecnologiasistech.hrworker.repositories.WorkerRepository;
 
